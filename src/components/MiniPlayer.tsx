02b0f9b9d64328ac8dc5d771ea9692898322672b
Aadi <aroraexe@users.noreply.github.com>
2026-07-31 23:13:19 +0530

    Make MiniPlayer draggable anywhere on screen

--- src/components/MiniPlayer.tsx
+++ src/components/MiniPlayer.tsx
@@ -26,7 +26,10 @@ export default function MiniPlayer() {
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] }}
-      className="fixed bottom-20 right-6 z-[999] flex flex-col items-end gap-3"
+      className="fixed bottom-20 right-6 z-[999] flex flex-col items-end gap-3 cursor-grab active:cursor-grabbing"
+      drag
+      dragMomentum={false}
+      dragElastic={0.1}
     >
       {/* Now Playing Tooltip */}
       <AnimatePresence>
